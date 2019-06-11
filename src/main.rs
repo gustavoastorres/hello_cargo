1730125e27df95dd2b3f0f3b8e056d271fa69460
Gustavo Torres <gustavo.torres@nokia.com>
2019-06-11 02:45:12 +0100

    Init version

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    //! this is a doc comment 
+    //! ffff //!
+    println!("Hello, cargo this is a new test!");
+    println!("I'm a rustacean", );
+    print!("hehehe\nhehehehe\n", );
+    let x = 5 + /* 90 + */ 5;
+    println!("Is `x` 10 or 100? x = {}", x);
+}
