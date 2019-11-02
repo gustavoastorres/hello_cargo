a89647e9ad2681c33366e14d97d0adc67ebf77e4
Gustavo <gustavo.torres@nokia.com>
2019-11-02 20:42:58 +0000

    another test

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     //! this is a doc comment 
     //! ffff //!
-    println!("Hello, cargo this is a new test!");
+    println!("Hello, cargo this is another test!");
     println!("I'm a rustacean", );
     print!("hehehe\nhehehehe\n", );
     let x = 5 + /* 90 + */ 5;
